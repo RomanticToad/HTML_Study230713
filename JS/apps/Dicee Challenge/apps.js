09f81af62bf0e868e54b5bcea59f8de454579e1d
김경태 <kkt8274@gmail.com>
2023-07-20 16:09:56 +0900

    오늘수업 백업

---
+++ JS/apps/Dicee Challenge/apps.js
@@ -0,0 +1,8 @@
+// // alert("서동옥 ");
+// const img1 = document.querySelector(".img1");
+// const img2 = document.querySelector(".img2");
+
+// img1.setAttribute("src", "images/dice6.png");
+// img2.setAttribute("src", "images/dice6.png");
+let age = prompt("나이를 입력해 주세요");
+alert(`당신의 나이는 ${age}입니다!`);
